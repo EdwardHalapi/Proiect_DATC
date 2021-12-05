266dfa7331d470f4781efd0378bcba807902c764
Edward <edwardhalapi@hsoftdev.org>
2021-12-05 17:03:50 +0200

    integrated the google maps api

--- citydanger/lib/screens/home_screen.dart
+++ citydanger/lib/screens/home_screen.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  final Completer<GoogleMapController> _controller = Completer();
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       appBar: AppBar(
         leading: IconButton(
             icon: const Icon(Icons.menu, color: Colors.white),
@@ -23,6 +37,15 @@ class HomePage extends StatelessWidget {
             letterSpacing: 0.5,
           ),
         ),
+      ),
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+        myLocationButtonEnabled: false,
+        zoomControlsEnabled: false,
       ),
     );
   }
